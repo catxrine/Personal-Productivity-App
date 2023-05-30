e09e78efda48f5e563fbbbc508627e5f84e1bd02
TeodoraNS <teodoranikiforova14@gmail.com>
2023-05-30 15:32:01 +0300

    ☕

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./store.jsx";
-import "./index.css";
+import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
